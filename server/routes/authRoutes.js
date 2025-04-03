0ecefd8abccc89582115616717b50b43a1f1422d
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-04-03 22:09:19 +0530

    feat: add validation middleware for user registration route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { registerUser, loginUser } from '../controllers/authController.js';
+import { validateUserRegistration, handleValidationErrors } from '../validators/validateUserRegistration.js';
 
 const   router = express.Router();
 
-router.post('/register', registerUser);
+router.post('/register', validateUserRegistration, handleValidationErrors, registerUser);
 router.post('/login', loginUser);
 
 export default router;
