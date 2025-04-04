ae3b58a57c494775e23d1b691f9544e4a98d93a2
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-04-04 20:58:25 +0530

    feat: update password validation to require strong passwords

--- server/validators/validateUserRegistration.js
+++ server/validators/validateUserRegistration.js
@@ -3,8 +3,7 @@ import { body, validationResult } from "express-validator";
 export const validateUserRegistration = [
     body('name', 'Name is Required, You can Leave it Empty').notEmpty(),
     body('email', 'Email Address You Provided Is Not A Valid Email').isEmail(),
-    body('password', 'Invalid Password! Make Sure It\'s Length Is More Than Or Equal To 8 Characters').isLength({min: 8}),
-    body('password', "Invalid Password! Make Sure It Contains Both Case Letters, Numbers, and Special Characters").matches(`/^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]))\S$/`)
+    body('password', "Sorry, Please make a Strong Password").isStrongPassword()
 ]
 
 export const handleValidationErrors = (req, res, next) => {
