bbccd05aab53ae923584f79cc70fd88c87e4f47f
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-04-04 21:01:42 +0530

    feat: enable timestamps for user model schema

--- server/models/User.js
+++ server/models/User.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true, maxLength: 50 },
   role: { type: String, enum: { values: ['customer', 'admin'], default: 'customer' } },
   phone: { type: String, maxLength: 10 },
-});
+}, {'timestamps': true});
 
 const User = mongoose.model('User', userSchema);
 
