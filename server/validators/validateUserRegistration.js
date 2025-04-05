b4455df2b181c990e80fa87133b83f46ff1d4efb
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-04-05 08:05:35 +0530

    fix: enhance user registration validation by adding trimming and improving error response handling

--- server/validators/validateUserRegistration.js
+++ server/validators/validateUserRegistration.js
@@ -1,20 +1,20 @@
 import { body, validationResult } from "express-validator";
 
 export const validateUserRegistration = [
-    body('name', 'Name is Required, You can Leave it Empty').notEmpty(),
-    body('email', 'Email Address You Provided Is Not A Valid Email').isEmail(),
-    body('password', "Sorry, Please make a Strong Password").isStrongPassword()
+    body('name', 'Name is Required, You can Leave it Empty').trim().notEmpty(),
+    body('email', 'Email Address You Provided Is Not A Valid Email').trim().isEmail(),
+    body('password', "Sorry, Please make a Strong Password").trim().isStrongPassword(),
+    body('*', "Sorry You Can't Play With Us. No Field Can Be Too Long In Length").isLength({'max': 100})
 ]
 
 export const handleValidationErrors = (req, res, next) => {
     const errors = validationResult(req)['errors']
     if(errors.length !== 0){
-        let message = []
+        const message = []
         errors.forEach(element => {
             message.push(element['msg'])
         });
-        message = message.join("\n")
-        return res.status(400).send(message)
+        return res.status(400).send(message[0])
     }
     next()
 }
