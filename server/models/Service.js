76b3445a82dc92cd836bf05cb055253f450ff35f
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-04-05 18:19:28 +0530

    fix: set default value for service description and update category enum options

--- server/models/Service.js
+++ server/models/Service.js
@@ -2,11 +2,11 @@ import mongoose from 'mongoose';
 
 const serviceSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  description: { type: String },
+  description: { type: String, default: "Not Available" },
   price: { type: Number, required: true },
   duration: { type: Number, required: true }, // in minutes
-  category: { type: String, enum: ['Hair', 'Skincare', 'Nails', 'Makeup'] },
-});
+  category: { type: String, enum: ['Hair', 'Skincare', 'Nails', 'Makeup', 'Other'], default: 'Other' },
+}, {'timestamps': true});
 
 const Service = mongoose.model('Service', serviceSchema);
 
