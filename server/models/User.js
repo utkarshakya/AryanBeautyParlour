de8ba6d32b5371621e551c917ff4db4169257d09
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-04-05 08:44:30 +0530

    fix: reorder role definition in user schema for clarity

--- server/models/User.js
+++ server/models/User.js
@@ -4,8 +4,8 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true, maxLength: 50 },
   email: { type: String, required: true, unique: true, maxLength: 100 },
   password: { type: String, required: true},
+  role: { type: String, enum: {values: ['customer', 'admin']}, default: 'customer' },
   phone: { type: String, maxLength: 10 },
-  role: { type: String, enum: { values: ['customer', 'admin'], default: 'customer' } },
 }, {'timestamps': true});
 
 const User = mongoose.model('User', userSchema);
