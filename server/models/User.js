ac98badf0feb02b4dd61933def26c63f60219cda
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-04-05 08:05:42 +0530

    fix: correct password field maxLength and reorder role definition in user schema

--- server/models/User.js
+++ server/models/User.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, maxLength: 50 },
   email: { type: String, required: true, unique: true, maxLength: 100 },
-  password: { type: String, required: true, maxLength: 50 },
-  role: { type: String, enum: { values: ['customer', 'admin'], default: 'customer' } },
+  password: { type: String, required: true},
   phone: { type: String, maxLength: 10 },
+  role: { type: String, enum: { values: ['customer', 'admin'], default: 'customer' } },
 }, {'timestamps': true});
 
 const User = mongoose.model('User', userSchema);
