606d2089f0cf3d35dae6163bf4e525020e6b6df2
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-04-11 15:13:08 +0530

    feat: implement routing in App component with Home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
+import Home from './pages/Home';
+// import Services from './pages/Services';
+// import About from './pages/About';
+// import Contact from './pages/Contact';
 
-export default function App(){
-    return (
-        <h1>Hello Universe</h1>
-    )
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/services" element={<Services />} /> */}
+        {/* <Route path="/about" element={<About />} /> */}
+        {/* <Route path="/contact" element={<Contact />} /> */}
+      </Routes>
+    </Router>
+  );
 }
