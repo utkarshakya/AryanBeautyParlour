02bfb651b76c3086a0a5b615528e169a0604a053
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-04-11 15:13:17 +0530

    feat: create Home component with Navbar and Hero sections

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      {/* <ServicesPreview /> */}
+      {/* Add more sections */}
+      {/* <Footer /> */}
+    </>
+  );
+}
