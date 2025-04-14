53f97c17741764941198cd2a2f9f235dc4557320
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-04-14 08:40:23 +0530

    feat: add logout route to authRoutes and clean up import statements

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { registerUser, loginUser } from '../controllers/authController.js';
-import {validateRegistration, validateLogin, handleValidationErrors} from './../validators/userValidator.js';
+import { registerUser, loginUser, logoutUser } from '../controllers/authController.js';
+import { validateRegistration, validateLogin, handleValidationErrors } from '../validators/userValidator.js';
+import { protect } from "../middlewares/auth.js"
 
-const   router = express.Router();
+const router = express.Router();
 
-router.post('/register',validateRegistration, handleValidationErrors, registerUser);
-router.post('/login',validateLogin, handleValidationErrors, loginUser);
+router.post('/register', validateRegistration, handleValidationErrors, registerUser);
+router.post('/login', validateLogin, handleValidationErrors, loginUser);
+router.post('/logout', protect, logoutUser);
 
 export default router;
