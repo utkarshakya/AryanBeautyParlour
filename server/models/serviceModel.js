cf46aed7c604266050772e1673d2e2b96942c7e5
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-04-14 09:31:37 +0530

    refactor: standardize code formatting and improve schema definitions in serviceModel and userModel

--- server/models/serviceModel.js
+++ server/models/serviceModel.js
@@ -1,13 +1,39 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const serviceSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, default: "Not Available" },
-  price: { type: Number, required: true },
-  duration: { type: Number, required: true }, // in minutes
-  category: { type: String, enum: ['Hair', 'Skincare', 'Nails', 'Makeup', 'Other'], default: 'Other' },
-}, {'timestamps': true});
+const serviceSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 50
+    },
+    description: {
+      type: String,
+      default: "Not Available",
+      maxLength: 100
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: [1, "Price must be at least 1"]
+    },
+    duration: {
+      // in minutes
+      type: Number,
+      required: true,
+      min: [1, "Duration must be at least 1"],
+      max: [300, "Duration must be less then 300"]
+    }, 
+    category: {
+      type: String,
+      enum: ["Hair", "Skincare", "Nails", "Makeup", "Other"],
+      default: "Other",
+    },
+  },
+  { timestamps: true }
+);
 
-const Service = mongoose.model('Service', serviceSchema);
+const Service = mongoose.model("Service", serviceSchema);
 
 export default Service;
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,13 +1,36 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true, maxLength: 50 },
-  email: { type: String, required: true, unique: true, maxLength: 100 },
-  password: { type: String, required: true},
-  role: { type: String, enum: ['customer', 'admin'], default: 'customer' },
-  phone: { type: String, maxLength: 10 },
-}, {'timestamps': true});
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      maxLength: 100,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["customer", "admin"],
+      default: "customer",
+    },
+    phone: {
+      type: String,
+      required: true,
+      match: [/^\d{10}$/, 'Please enter a valid 10-digit mobile number']
+    },
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
