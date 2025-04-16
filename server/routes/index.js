b4abc6dbc9aa75c9b750bfeba663bc9272d70003
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-04-16 10:50:36 +0530

    feat: consolidate route imports in server.js for improved organization

---
+++ server/routes/index.js
@@ -0,0 +1,13 @@
+import appointmentRoutes from "./appointmentRoutes.js";
+import authRoutes from "./authRoutes.js";
+import notificationRoutes from "./notificationRoutes.js";
+import paymentRoutes from "./paymentRoutes.js";
+import serviceRoutes from "./serviceRoutes.js";
+
+export {
+  appointmentRoutes,
+  authRoutes,
+  notificationRoutes,
+  paymentRoutes,
+  serviceRoutes,
+};
--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
-import express from 'express';
-import cors from 'cors';
-import config from './config/env.js';
-import connectDB from './config/db.js';
-import authRoutes from './routes/authRoutes.js';
-import serviceRoutes from './routes/serviceRoutes.js';
-import appointmentRoutes from './routes/appointmentRoutes.js';
-import paymentRoutes from './routes/paymentRoutes.js';
+import express from "express";
+import cors from "cors";
+import config from "./config/env.js";
+import connectDB from "./config/db.js";
+import {
+  authRoutes,
+  serviceRoutes,
+  appointmentRoutes,
+  paymentRoutes,
+  notificationRoutes,
+} from "./routes";
 
 // Initialize Express
 const app = express();
@@ -14,13 +17,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/services', serviceRoutes);
-app.use('/api/appointments', appointmentRoutes);
-app.use('/api/payment', paymentRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/services", serviceRoutes);
+app.use("/api/appointments", appointmentRoutes);
+app.use("/api/payment", paymentRoutes);
+app.use("/api/notification", notificationRoutes);
 
 // Test route
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).send("Beauty Is Applied");
 });
 
@@ -28,9 +32,9 @@ app.get('/', (req, res) => {
 connectDB()
   .then(() => {
     app.listen(config.port, () => {
-      console.log(`Server running on port ${config.port}`)
-    })
+      console.log(`Server running on port ${config.port}`);
+    });
   })
   .catch(() => {
-    console.log(`Sorry! ${err}`)
-  })
+    console.log(`Sorry! ${err}`);
+  });
