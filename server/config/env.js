85230bb6892ba0a9f6b00ee1eeb1a99dba156108
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-04-17 09:28:04 +0530

    refactor: reorganize environment configuration structure for clarity and consistency

--- server/config/env.js
+++ server/config/env.js
@@ -3,14 +3,17 @@ dotenv.config();
 
 export default {
   port: process.env.PORT,
-  mongodbUri: process.env.MONGODB_URI,
   jwtSecret: process.env.JWT_SECRET,
-  razorpayId: process.env.RAZORPAY_KEY_ID,
-  razorpaySecret: process.env.RAZORPAY_KEY_SECRET,
-  cloudinary: {
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+  mongodbUri: process.env.MONGODB_URI,
+  redis: {
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+  razorpay: {
+    id: process.env.RAZORPAY_KEY_ID,
+    secret: process.env.RAZORPAY_KEY_SECRET,
   },
   smtp: {
     host: process.env.SMTP_HOST,
@@ -18,5 +21,10 @@ export default {
     username: process.env.SMTP_USERNAME,
     password: process.env.SMTP_PASSWORD,
     from: process.env.SMTP_FROM,
+  },
+  cloudinary: {
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
   },
 };
